1e64c1dc0aa2c9d8511d8728affe5795643fe68a
Hari Nallaparaju <hari.nallaparaju@gmail.com>
2024-11-09 21:19:38 +0530

    added dashboard

--- components/SupermarketAnalytics.js
+++ components/SupermarketAnalytics.js
@@ -151,7 +151,7 @@ const SupermarketAnalytics = () => {
 
       {/* Footer */}
       <div className="flex items-center gap-2 text-sm text-gray-500">
-        <Clock className="w-4 h-4" />
+        <Clock className="w-4 h-4 " />
         <span>Data updates every 5 seconds</span>
       </div>
     </div>
